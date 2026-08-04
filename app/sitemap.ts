a3e1d383fd3c1364358974669bfcc35046dd5ccd
justinerothrd <justinerothrd@gmail.com>
2026-08-04 00:52:38 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,38 +6,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: siteUrl,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${siteUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${siteUrl}/services`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${siteUrl}/what-to-expect`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.7,
     },
     {
       url: `${siteUrl}/groups`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.6,
     },
     {
       url: `${siteUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.7,
     },
   ];
