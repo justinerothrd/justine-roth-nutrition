9d5eb02372f04b43ca65f16e297c6d9421017fd0
justinerothrd <justinerothrd@gmail.com>
2026-08-04 14:00:05 -0400

    Update layout.tsx

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Contact Justine Roth, MS, RD, CEDS-C",
+
   description:
-    "Contact Justine Roth Nutrition to schedule an appointment for eating disorder nutrition counseling in New York City or virtually.",
+    "Schedule a consultation for eating disorder nutrition counseling in New York City or virtually throughout New York.",
+
   alternates: {
-    canonical: "/contact",
+    canonical: "https://justinerothnutrition.com/contact",
   },
 };
 
