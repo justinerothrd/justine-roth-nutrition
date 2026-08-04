9a06b9924fb76d2b9dd9f0ad8b5b66253189e1ae
justinerothrd <justinerothrd@gmail.com>
2026-08-04 00:35:59 -0400

    Update robots.ts to use siteUrl constant

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,15 @@
 import type { MetadataRoute } from "next";
 
+const siteUrl = "https://justinerothnutrition.com";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/api/"],
     },
-    sitemap: "https://justinerothnutrition.com/sitemap.xml",
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   };
 }
